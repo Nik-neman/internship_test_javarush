7eaf6c32acf146ff7b21b3a3f8b893ea10ab51b0
Nikolai <nik-neman@mail.ru>
2022-09-03 10:07:44 +0200

    add PlayerRepository and PlayerService

--- src/main/java/com/game/controller/PlayerController.java
+++ src/main/java/com/game/controller/PlayerController.java
@@ -1,4 +1,13 @@
 package com.game.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlayerController {
+
+    @RequestMapping("/")
+    public String showForm(){
+        return "index";
+    }
 }
